4bb7f546434b969246449ab771af1c01206a70dc
battery-staple <56455596+battery-staple@users.noreply.github.com>
2022-08-25 20:04:48 -0400

    Added mapBetweenRanges

---
+++ src/main/kotlin/frc/chargers/utils/math/MapBetweenRanges.kt
@@ -0,0 +1,9 @@
+package frc.chargers.utils.math
+
+public fun Double.mapBetweenRanges(from: ClosedRange<Double>, to: ClosedRange<Double>): Double {
+    require(this in from) { "Value must be within initial range." }
+
+    val proportionIntoRange: Double = (this - from.start) / (from.endInclusive - from.start)
+    val distanceIntoToRange: Double = proportionIntoRange * (to.endInclusive - from.start)
+    return to.start + distanceIntoToRange
+}
