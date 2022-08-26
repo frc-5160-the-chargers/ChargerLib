b1ccb0abd83b60b7750e118d8028a3f2427f30ba
battery-staple <56455596+battery-staple@users.noreply.github.com>
2022-08-25 20:05:02 -0400

    Added ChargerController

---
+++ src/main/kotlin/frc/chargers/hardware/inputdevices/ChargerController.kt
@@ -0,0 +1,20 @@
+package frc.chargers.hardware.inputdevices
+
+import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj2.command.button.Button as WPIlibCommandButton
+import edu.wpi.first.wpilibj2.command.button.JoystickButton
+import kotlin.math.abs
+
+public open class ChargerController(
+    port: Int,
+    private val deadband: Double = 0.0,
+    private val defaultAxisThreshold: Double = 0.5
+) : XboxController(port) {
+    protected fun button(button: Button): WPIlibCommandButton =
+        JoystickButton(this, button.value)
+    protected fun button(axis: Axis, threshold: Double = defaultAxisThreshold): WPIlibCommandButton =
+        JoystickAnalogButton(this, axis.value, threshold)
+
+    protected fun Double.withDeadband(): Double =
+        if (abs(this) <= deadband) { 0.0 } else { this }
+}
