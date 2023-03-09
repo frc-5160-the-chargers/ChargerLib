dca54eede78efc63cccf8ef08ae9266d34d4a224
battery-staple <56455596+battery-staple@users.noreply.github.com>
2023-03-08 20:08:23 -0500

    Fixed AnalogPotentiometerPositionEncoder

--- src/main/kotlin/frc/chargers/hardware/sensors/encoders/AnalogPotentiometerEncoder.kt
+++ src/main/kotlin/frc/chargers/hardware/sensors/encoders/AnalogPotentiometerEncoder.kt
@@ -8,8 +8,10 @@ import com.batterystaple.kmeasure.units.degrees
 import edu.wpi.first.wpilibj.AnalogInput
 import edu.wpi.first.wpilibj.AnalogPotentiometer
 
-public class AnalogPotentiometerPositionEncoder(input: AnalogInput, scale: Double = 1.0, offset: Angle = 0.degrees) :
-    AnalogPotentiometer(input, scale, offset.inUnit(Degrees)), PositionEncoder {
+public class AnalogPotentiometerPositionEncoder(input: AnalogInput, fullRange: Angle, offset: Angle = 0.degrees) :
+    AnalogPotentiometer(input, fullRange.inUnit(Degrees), offset.inUnit(Degrees)), PositionEncoder {
+    public constructor(channel: Int, fullRange: Angle, offset: Angle = 0.degrees) : this(AnalogInput(channel), fullRange, offset)
+
     override val angularPosition: Angle
         get() = get().ofUnit(Degrees)
 }
