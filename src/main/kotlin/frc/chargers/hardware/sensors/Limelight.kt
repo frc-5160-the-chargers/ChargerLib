0e82211c8141338b9a36373b2dee53e263756279
Daniel Chen <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-03-16 19:40:10 -0400

    Create Limelight.kt

---
+++ src/main/kotlin/frc/chargers/hardware/sensors/Limelight.kt
@@ -0,0 +1,61 @@
+package frc.chargers.hardware.sensors;
+
+import com.batterystaple.kmeasure.units.*
+import com.batterystaple.kmeasure.units.degrees
+import com.batterystaple.kmeasure.units.meters
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.networktables.NetworkTable
+import edu.wpi.first.networktables.NetworkTableEntry
+import edu.wpi.first.networktables.NetworkTableInstance
+
+// a wrapper for the limelight; makes it easier to use
+class Limelight(noiseFilterThreshold: Double = 0.03, lensHeight: Distance? = null, mountAngle: Angle? = null){
+    
+    public var previousValues = mapOf("tx" to 0.0,"ty" to 0.0, "ta" to 0.0)
+
+    // gets a limelight entry.
+    // simply makes the limelight entries for tx, ty and ta more consistent by filtering out "noise"
+    fun getEntry(entry:String): Double{
+        var value: Double = NetworkTableInstance.getDefault().getTable("limelight").getEntry(entry).getDouble(0.0)
+        if (entry == "tx" || entry == "ty" || entry == "ta"){
+            if (value-previousValues[entry] < noiseFilterThreshold){
+                value = entry
+            }else{
+                previousValues[entry] = value
+            }
+        }
+        return value
+    }
+    /* */
+    fun setProperty(property:String,value:Double){
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry(property).setNumber(value)
+    }
+
+    fun setPipeline(pipeline: Int){
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline)
+    }
+
+    /*
+    gets horizontal distance.
+    How to use:
+    limeight.getHorizontalDistance(5.meters)
+    obviously returns in meters
+     */
+    fun getHorizontalDistance(targetHeight: Distance): Double{
+        if (lensHeight == null || mountAngle == null){return}
+        var angleToGoal: Angle = (mountAngle + getEntry("ty").inUnit(degrees))
+        return (targetHeight.meters/Math.tan(angleToGoal.radians))
+    }
+    
+    /*
+    gets diagonal distance from lens to target.
+    How to use:
+    limelight.getDistance(5.meters)
+    obviously returns in meters
+     */
+    fun getDistance(targetHeight: Distance): Double{
+        return Math.sqrt(getHorizontalDistance(targetHeight).pow(2.0) + targetHeight.meters.pow(2.0))
+    }    
+
+    
+}
