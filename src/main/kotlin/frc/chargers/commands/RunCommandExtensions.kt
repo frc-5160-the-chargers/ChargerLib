7ef9cd11d0101dde4eb11ac94054b4be4e0f0f98
Daniel Chen <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-03-16 20:57:16 -0400

    Update RunCommandExtensions.kt

--- src/main/kotlin/frc/chargers/commands/RunCommandExtensions.kt
+++ src/main/kotlin/frc/chargers/commands/RunCommandExtensions.kt
@@ -10,6 +10,12 @@ import edu.wpi.first.wpilibj2.command.Subsystem
 public fun RunCommand(vararg subsystems: Subsystem, toRun: () -> Unit): RunCommand =
     RunCommand(toRun, *subsystems)
 
+/* 
+ *   An alternative way to define RunCommands that is more in line with the buildCommand commands.
+ */
+public fun RunForeverCommand(vararg subsystems: Subsystem, toRun: () -> Unit): InstantCommand = 
+    RunCommand(*subsystems,toRun)
+
 /**
  * A utility function for setting the default [Command] of a [Subsystem] to a [RunCommand]
  * in a shorter and easier way.
