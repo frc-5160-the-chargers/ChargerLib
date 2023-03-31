24f1ed27928be9e947275c7e7e58a05b67feb794
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-03-31 10:22:37 -0400

    Improved Limelight.kt

--- src/main/kotlin/frc/chargers/hardware/sensors/DriverCamManager.kt
+++ src/main/kotlin/frc/chargers/hardware/sensors/DriverCamManager.kt
@@ -6,15 +6,14 @@ import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.cameraserver.CameraServer
 
 public class DriverCamManager(
-    public val totalDriverCams: Int = 0,
-    public val defaultResWidth: Int = 720,
-    public val defaultResHeight: Int = 1280){
-    internal var driverCameras = Array(totalDriverCams){i -> CameraServer.startAutomaticCapture(i)}.toList()
-    internal val cameraSelector: NetworkTableEntry = NetworkTableInstance.getDefault().getTable("").getEntry("CameraSelection")
+    public val totalDriverCams: Int = 1,
+    public val defaultResWidth: Int,
+    public val defaultResHeight: Int){
+    private var driverCameras = List(totalDriverCams){ i -> CameraServer.startAutomaticCapture(i)}
+    private val cameraSelector: NetworkTableEntry = NetworkTableInstance.getDefault().getTable("").getEntry("CameraSelection")
 
     init{
-        driverCameras = Array(totalDriverCams){i -> CameraServer.startAutomaticCapture(i)}.toList()
-        for (i in driverCameras){i.setResolution(defaultResWidth,defaultResHeight)}
+        for (driverCamera in driverCameras){driverCamera.setResolution(defaultResWidth,defaultResHeight)}
     }
 
     public fun switchToCamera(id: Int){
--- src/main/kotlin/frc/chargers/hardware/sensors/Limelight.kt
+++ src/main/kotlin/frc/chargers/hardware/sensors/Limelight.kt
@@ -1,6 +1,7 @@
 package frc.chargers.hardware.sensors
 
 import com.batterystaple.kmeasure.quantities.Angle
+import com.batterystaple.kmeasure.quantities.Area
 import com.batterystaple.kmeasure.quantities.Distance
 import com.batterystaple.kmeasure.quantities.inUnit
 import com.batterystaple.kmeasure.units.*
@@ -18,67 +19,174 @@ import kotlin.math.tan
 public class Limelight(public val noiseFilterThreshold: Double = 0.04,
                        public val lensHeight: Distance? = null,
                        public val mountAngle: Angle? = null){
-    
-    private var previousValues: MutableMap<String, Double> = mutableMapOf("tx" to 0.0,"ty" to 0.0, "ta" to 0.0)
+    private var nt: NetworkTable = NetworkTableInstance.getDefault().getTable("limelight")
 
-    // gets a limelight entry.
-    // simply makes the limelight entries for tx, ty and ta more consistent by filtering out "noise"
-    // most reliable if used in periodic loop or used in constant loop.
-    public fun getEntry(entry:String,filterValue:Boolean = (noiseFilterThreshold!=0.0)): Double{
-        var value: Double = NetworkTableInstance.getDefault().getTable("limelight").getEntry(entry).getDouble(0.0)
-        if (!filterValue){
-            return value
-        }else if (entry == "tx" || entry == "ty" || entry == "ta"){
-            if (value - previousValues[entry]!! < noiseFilterThreshold){
-                value = (value + previousValues[entry]!!)/2
+    // NOTE: This part is different than the networktables api!!!!!!
+    public var pipeline: Int
+        get() = nt.getEntry("getpipe").getDouble(0.0) as Int
+        set(value){
+            nt.getEntry("pipeline").setNumber(value)
+        }
+
+    // alternative to use base tv.
+    public val hasTarget: Boolean
+        get() = nt.getEntry("tv").getDouble(0.0) == 1.0
+
+    private var previousTX: Double = 0.0
+    private var previousTY: Double = 0.0
+    private var previousTA: Double = 0.0
+
+    public val tx: Angle
+        get(){
+            var value: Double = nt.getEntry("tx").getDouble(0.0)
+
+            if (value - previousTX < noiseFilterThreshold){
+                return ((value + previousTX)/2).degrees
             }else{
-                previousValues[entry] = value
+                previousTX = value
+                return value.degrees
             }
         }
-        return value
+    public val ty: Angle
+        get(){
+            var value: Double = nt.getEntry("ty").getDouble(0.0)
+
+            if (value - previousTY < noiseFilterThreshold){
+                return ((value + previousTY)/2).degrees
+            }else{
+                previousTY = value
+                return value.degrees
+            }
+        }
+    public val ta: Double
+        get(){
+            var value: Double = nt.getEntry("ta").getDouble(0.0)
+
+            if (value - previousTA < noiseFilterThreshold){
+                return (value + previousTA)/2
+            }else{
+                previousTA = value
+                return value
+            }
+        }
+    public val tv: Double
+        get() = nt.getEntry("tv").getDouble(0.0)
+    public val tl: Double
+        get() = nt.getEntry("tl").getDouble(0.0)
+    public val cl: Double
+        get() = nt.getEntry("cl").getDouble(0.0)
+    public val tshort: Double
+        get() = nt.getEntry("tshort").getDouble(0.0)
+    public val tlong: Double
+        get() = nt.getEntry("tlong").getDouble(0.0)
+    public val thor: Double
+        get() = nt.getEntry("thor").getDouble(0.0)
+    public val tvert: Double
+        get() = nt.getEntry("tvert").getDouble(0.0)
+    public val json: Double
+        get() = nt.getEntry("json").getDouble(0.0)
+    public val tclass: Double
+        get() = nt.getEntry("tclass").getDouble(0.0)
+    public val tc: Double
+        get() = nt.getEntry("tc").getDouble(0.0)
+
+
+    // NOTE: the array entry is auto converted into a list here, cuz lists are better
+    public val botpose: List<Double>
+        get() = nt.getEntry("botpose").getDoubleArray(Array(6){0.0}).toList()
+
+    public val botpose_wpiblue: List<Double>
+        get() = nt.getEntry("botpose_wpiblue").getDoubleArray(Array(6){0.0}).toList()
+
+    public val botpose_wpired: List<Double>
+        get() = nt.getEntry("botpose_wpired").getDoubleArray(Array(6){0.0}).toList()
+
+    public val camerapose_targetspace: List<Double>
+        get() = nt.getEntry("botpose").getDoubleArray(Array(6){0.0}).toList()
+
+    public val targetpose_cameraspace: List<Double>
+        get() = nt.getEntry("botpose").getDoubleArray(Array(6){0.0}).toList()
+
+    public val targetpose_robotspace: List<Double>
+        get() = nt.getEntry("botpose").getDoubleArray(Array(6){0.0}).toList()
+
+    public val camerapose_robotspace: List<Double>
+        get() = nt.getEntry("botpose").getDoubleArray(Array(6){0.0}).toList()
+
+    public val tid: List<Double>
+        get() = nt.getEntry("tid").getDoubleArray(Array(6){0.0}).toList()
+
+
+    public var ledMode: Int = 0
+        set(value){
+            field = value
+            nt.getEntry("ledMode").setNumber(value)
+        }
+
+    public var camMode: Int = 0
+        set(value){
+            field = value
+            nt.getEntry("camMode").setNumber(value)
+        }
+
+    public var stream: Int = 0
+        set(value){
+            field = value
+            nt.getEntry("stream").setNumber(value)
+
+        }
+
+    public var crop: List<Double> = listOf(-1.0,1.0,-1.0,1.0)
+        set(value){
+            field = value
+            nt.getEntry("crop").setDoubleArray(value.toTypedArray())
+        }
+
+    public fun snapshot(){
+        nt.getEntry("snapshot").setNumber(1)
     }
+
+    public fun resetSnapshotMode(){
+        nt.getEntry("snapshot").setNumber(1)
+    }
+
+
+    public fun getRawEntry(entry:String): Double = NetworkTableInstance.getDefault().getTable("limelight").getEntry(entry).getDouble(0.0)
+
+
+
     
     // sets a limelight property.
-    public fun setProperty(property:String,value:Double){
+    public fun setRawProperty(property:String,value:Double){
         NetworkTableInstance.getDefault().getTable("limelight").getEntry(property).setNumber(value)
     }
 
-    // sets a limelight pipeline. accepts int instead of double.
-    public fun setPipeline(pipeline: Int){
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline)
-    }
 
-    // values must range between -1 and 1.
-    // usage: limelight.setCrop(xRange = arrayOf(-0.5,0.5), yRange = arrayOf(-0.5,0.5))
-    public fun setCrop(xRange: List<Double> = listOf(-1.0,1.0), yRange: List<Double> = listOf(-1.0,1.0)){
-        val arrayToSend: Array<Double> = arrayOf(xRange[0],xRange[1],yRange[0],yRange[1])
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("crop").setDoubleArray(arrayToSend)
-    }
 
     // gets an array property instead of a regular one. useful for botpose stuff.
     // note: converts to lists cuz those are better.
-    public fun getListProperty(property: String): List<Double> = NetworkTableInstance.getDefault().getTable("limelight").getEntry(property).getDoubleArray(Array<Double>(6){0.0}).toList()
+    public fun getRawListProperty(property: String): List<Double> = NetworkTableInstance.getDefault().getTable("limelight").getEntry(property).getDoubleArray(Array<Double>(6){0.0}).toList()
 
     /*
     gets horizontal distance.
     How to use:
-    limeight.getHorizontalDistance(5.meters)
+    limeight.getHorizontalDistance(5.meters).inUnit(meters)
     obviously returns in meters
      */
-    public fun getHorizontalDistance(targetHeight: Distance): Double{
-        if (lensHeight == null || mountAngle == null){return 0.0}
-        var angleToGoal: Angle = (mountAngle + getEntry("ty").degrees)
-        return (targetHeight.inUnit(meters)/ tan(angleToGoal.inUnit(radians)))
+    public fun getHorizontalDistance(targetHeight: Distance): Distance{
+        if (lensHeight == null || mountAngle == null){return 0.0.meters}
+        var angleToGoal: Angle = (mountAngle + ty)
+        return (targetHeight.inUnit(meters)/ tan(angleToGoal.inUnit(radians))).meters
     }
     
     /*
     gets diagonal distance from lens to target.
     How to use:
-    limelight.getDistance(5.meters)
+    limelight.getDistance(5.meters).inUnit(meters)
     obviously returns in meters
      */
-    public fun getDistance(targetHeight: Distance): Double{
-        return sqrt(getHorizontalDistance(targetHeight).pow(2.0) + targetHeight.inUnit(meters).pow(2.0))
+    public fun getDistance(targetHeight: Distance): Distance{
+        return sqrt(getHorizontalDistance(targetHeight).inUnit(meters).pow(2.0) + targetHeight.inUnit(meters).pow(2.0)).meters
     }    
 
     
