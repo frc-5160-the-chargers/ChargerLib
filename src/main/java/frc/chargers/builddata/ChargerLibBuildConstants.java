3b4b9ef5945581d673a6af0a6d73069b483de387
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-10-16 10:45:48 -0400

    Quick bugfixes

--- src/main/java/frc/chargers/builddata/ChargerLibBuildConstants.java
+++ src/main/java/frc/chargers/builddata/ChargerLibBuildConstants.java
@@ -7,12 +7,12 @@ public final class ChargerLibBuildConstants {
 	public static final String MAVEN_GROUP = "com.batterystaple";
 	public static final String MAVEN_NAME = "ChargerLib";
 	public static final String VERSION = "1.0.0-SNAPSHOT";
-	public static final int GIT_REVISION = 140;
-	public static final String GIT_SHA = "2b469ae4a96c54e8afd7ec1b7b8a7cfa26fa1e19";
-	public static final String GIT_DATE = "2023-10-16T02:17:15Z";
+	public static final int GIT_REVISION = 141;
+	public static final String GIT_SHA = "89b66d6d1b4d65e476743475a251da3dba576ecb";
+	public static final String GIT_DATE = "2023-10-15T21:21:04Z";
 	public static final String GIT_BRANCH = "AdvantageKit";
-	public static final String BUILD_DATE = "2023-10-16T02:20:20Z";
-	public static final long BUILD_UNIX_TIME = 1697422820477L;
+	public static final String BUILD_DATE = "2023-10-16T09:43:44Z";
+	public static final long BUILD_UNIX_TIME = 1697467424068L;
 	public static final int DIRTY = 1;
 
 	private ChargerLibBuildConstants(){}
--- src/main/kotlin/frc/chargers/hardware/sensors/IMUSim.kt
+++ src/main/kotlin/frc/chargers/hardware/sensors/IMUSim.kt
@@ -17,7 +17,7 @@ import frc.chargers.wpilibextensions.kinematics.yVelocity
  * as a stand-in for values.
  */
 public class IMUSim(
-    private val headingProviderImpl: HeadingProvider,
+    private val headingProviderImpl: HeadingProvider = HeadingProvider.Static(),
     private val getChassisSpeeds: () -> ChassisSpeeds = { ChassisSpeeds(0.0,0.0,0.0) }
 ): IMU{
     override fun reset() {
