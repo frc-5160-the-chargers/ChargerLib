1c584b16a25a936785052854ad9db0886d40c577
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-12-11 15:39:26 -0500

    More small changes

--- src/main/kotlin/frc/chargers/hardware/sensors/cameras/vision/VisionPipeline.kt
+++ src/main/kotlin/frc/chargers/hardware/sensors/cameras/vision/VisionPipeline.kt
@@ -15,29 +15,9 @@ import kotlin.math.sqrt
 public interface VisionPipeline<R: VisionResult> {
 
     /**
-     * The pipeline index.
+     * The pipeline index of the [VisionPipeline].
      */
     public val index: Int
-
-    /**
-     * resets the camera that the [VisionPipeline] belongs to in order to return proper results.
-     */
-    public fun reset(){}
-
-
-    /**
-     * Fetches the full vision data of the [VisionPipeline]; These are all from the exact same timestamp.
-     */
-    public val visionData: NonLoggableVisionData<R>?
-
-    /**
-     * Fetches the current best target of the [VisionPipeline].
-     *
-     * The values fetched here are not nessecarily from the exact same timestamp.
-     */
-    public val bestTarget: R?
-        get() = visionData?.bestTarget
-
 
     /**
      * How high the vision camera's lens is from the ground.
@@ -48,6 +28,33 @@ public interface VisionPipeline<R: VisionResult> {
      * The mount angle describes how many degrees the vision camera is from perfectly vertical.
      */
     public val mountAngle: Angle
+
+    /**
+     * resets the camera that the [VisionPipeline] belongs to in order to return proper results.
+     * This can include setting the overall camera's pipeline index to the index specified.
+     */
+    public fun reset(){
+        println("A generic vision camera's pipeline has just tried to reset the pipeline to $index.")
+        println("This pipeline did not provide an implementation of the reset() method; use override fun reset(){} if this is necessary.")
+    }
+
+    /**
+     * Fetches the full vision data of the [VisionPipeline]; These are all from the exact same timestamp.
+     * A null value represents the vision data being invalid/the pipeline having no available targets.
+     */
+    public val visionData: NonLoggableVisionData<R>?
+
+
+
+
+
+    /**
+     * Fetches the current best target of the [VisionPipeline].
+     *
+     * The values fetched here are not nessecarily from the exact same timestamp.
+     */
+    public val bestTarget: R?
+        get() = visionData?.bestTarget
 
     public fun horizontalDistanceToTarget(
         targetHeight: Distance, targetPitch: Angle = Angle(0.0)
